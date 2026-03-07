a23829fdfd98b3f4c167a8f5ecca8cbd54b29bdf
jdaubman13 <161610147+jdaubman13@users.noreply.github.com>
2026-03-06 23:22:18 -0500

    Create test_health.py

---
+++ test_health.py
@@ -0,0 +1,14 @@
+import pytest
+from health import *
+
+def test_take_damage(player):
+    result = take_damage(player, 30)
+    assert result["health"] == 50
+
+def test_heal_adds_health(player):
+    result = heal(player, 20)
+    assert result["health"] == 100
+
+def test_player_dies(player):
+    result = take_damage(player, 100)
+    assert result["alive"] == False
