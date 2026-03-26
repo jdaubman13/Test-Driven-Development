f67e8523c72dc94463c2b62ae0f0f9e3f27acbac
jdaubman13 <161610147+jdaubman13@users.noreply.github.com>
2026-03-26 08:24:00 -0400

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -3,21 +3,22 @@ def add_item(inventory, item):
         raise ValueError("Please add a valid item to inventory")
     elif len(inventory["items"]) >= inventory["capacity"]:
         raise ValueError("Inventory is at max capacity")
-    elif inventory["locked"] == True:
+    elif inventory["locked"] is True:
         return inventory
     else:
         inventory["items"].append(item)
         return inventory
 
+
 def remove_item(inventory, item):
     if item not in inventory["items"]:
         raise ValueError("Inventory item not in inventory")
-    elif inventory["locked"] == True:
+    elif inventory["locked"] is True:
         return inventory
     else:
         inventory["items"].remove(item)
         return inventory
 
+
 def get_item_count(inventory):
     return len(inventory["items"])
-
